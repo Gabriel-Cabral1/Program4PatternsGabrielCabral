06efbf20499f4cfe82018da90637a59ef2857477
gabriel <gc25719@email.vccs.edu>
2025-11-12 21:36:10 -0500

    .

--- Program4PatternsGabrielCabral.cpp
+++ Program4PatternsGabrielCabral.cpp
@@ -1,20 +1,36 @@
-// Program4PatternsGabrielCabral.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+ Project Name : Program4PatternsGabrielCabral
+ File Name : Program4PatternsGabrielCabral.cpp
+ Programmer : Gabriel Cabral
+ Date : 11/11/25
+ Requirements:
+ Use nested or sequential loops to generate and format character-based output.
+ Control repetition and alignment to match required patterns.
+ Develop confidence with loop structure variations.
+*/
+/*
+Print + for pattern A left
+Print  + for pattern B right
+*/
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << setw(10) << left << "Pattern A" << setw(12) << left << "Pattern B" << endl;
+    for (int i=1; i<=10; i++) {
+
+        for (int j = 1; j <= i; j++) {
+            cout << "+";
+        }
+
+        cout << setw(10 - i) << "";
+
+        for (int j=1; j<=11-i; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
